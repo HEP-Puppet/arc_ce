e0b0a7f5e136127f955c6f8550f48b9663a7b971
kreczko <lkreczko@gmail.com>
2013-07-16 16:00:12 +0100

    adding reference configuration, template and config module

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,14 @@
-class arc_ce::config {
-  
+class arc_ce::config (
+  $node_cpu          = 2,
+  $resource_location = "Lund, Sweden",
+  $mail              = "gridmaster@hep.lu.se",
+  $lrms              = "fork",
+  $session_dir       = "/tmp/grid",
+  $control_dir       = "/tmp/jobstatus",
+  $cache_dir         = "/tmp/cache",) {
+  file { '/etc/arc.conf':
+    ensure  => present,
+    content => template("${module_name}/sender.cfg.erb"),
+    require => Package['nordugrid-arc-compute-element'],
+  }
 }
