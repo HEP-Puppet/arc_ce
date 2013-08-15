9c47f7d1e7e99fa73751c179d3f5b68051669b70
Luke Kreczko <lkreczko@gmail.com>
2013-08-15 14:20:12 +0100

    adding firewall settings and dependencies

---
+++ manifests/firewall.pp
@@ -0,0 +1,46 @@
+class arc_ce::firewall {
+  firewall { '200 For the web service interface':
+    action => accept,
+    proto  => tcp,
+    state  => NEW,
+    port   => [
+      80,
+      443],
+  }
+
+  firewall { '201 For the bdii service interface':
+    action => accept,
+    proto  => tcp,
+    state  => NEW,
+    port   => 2170,
+  }
+
+  firewall { '202 For HTTPg':
+    action => accept,
+    proto  => tcp,
+    state  => NEW,
+    port   => 8443,
+  }
+
+  firewall { '203 For the GridFTP service interface':
+    action => accept,
+    proto  => tcp,
+    state  => NEW,
+    port   => [
+      2811,
+      2119],
+  }
+
+  firewall { '204 For GridFTP data channels':
+    action => accept,
+    proto  => tcp,
+    state  => NEW,
+    dport  => '9000-9300',
+  }
+
+  firewall { '205 For the LDAP service interface':
+    action => accept,
+    proto  => tcp,
+    port   => 2135,
+  }
+}
