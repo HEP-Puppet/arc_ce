6fb5a4efa04440ba93e7152541d806bf5b125629
Luke Kreczko <lkreczko@gmail.com>
2013-08-23 11:52:44 +0100

    formatting for e259e46

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,12 @@
 class arc_ce::config (
-  $cluster_alias = 'MINIMAL Computing Element',
-  $cluster_comment = 'This is a minimal out-of-box CE setup',
+  $cluster_alias     = 'MINIMAL Computing Element',
+  $cluster_comment   = 'This is a minimal out-of-box CE setup',
   $node_cpu          = 2,
   $resource_location = 'Lund, Sweden',
   $mail              = 'gridmaster@hep.lu.se',
   $lrms              = 'fork',
   $enable_glue1      = false,
-  $enable_glue2      = true,  
+  $enable_glue2      = true,
   $log_directory     = '/var/log/arc',
   $run_directory     = '/var/run/arc',
   $domain_name       = 'ARC-TESTDOMAIN') {
