ef8f93c4ffb7f0c71da2ce2864de94c17e9f7f57
kashif74 <kashif.alig@gmail.com>
2014-03-19 12:45:17 +0000

    empty RTEs script for atlas

--- manifests/config.pp
+++ manifests/config.pp
@@ -121,7 +121,23 @@ class arc_ce::config (
     '/etc/arc/',
     '/etc/arc/runtime/',
     '/etc/arc/runtime/ENV']: ensure => directory, }
+  
+ # Create empty ATLAS-SITE-LCG  for ATLAS prd jobs
 
+  file { [ '/etc/arc/runtime/APPS',
+           '/etc/arc/runtime/APPS/HEP',] :
+         ensure => directory,
+         require => File['/etc/arc/runtime'],
+       } 
+  
+  file { '/etc/arc/runtime/APPS/HEP/ATLAS-SITE-LCG':
+      ensure  => present,
+      source  => "puppet:///modules/${module_name}/RTEs/ATLAS-SITE-LCG",
+      require => File['/etc/arc/runtime/APPS/HEP'],
+    }
+
+  
+ 
   # plugin to set a default runtime environment
   file { '/usr/local/bin/default_rte_plugin.py':
     ensure => present,
@@ -134,7 +150,7 @@ class arc_ce::config (
     file { '/etc/arc/runtime/ENV/GLITE':
       ensure  => present,
       source  => "puppet:///modules/${module_name}/RTEs/GLITE",
-      require => File['/etc/arc/runtime/ENV'],
+      require => File['/etc/arc/runtime/'],
     }
 
     file { '/etc/arc/runtime/ENV/PROXY':
