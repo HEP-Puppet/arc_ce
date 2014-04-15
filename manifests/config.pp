bf33c56818f28ea13d696600dbb5195b65a38cc5
kashif74 <kashif.alig@gmail.com>
2014-04-15 11:58:37 +0100

    Fixed dependency for /ENV/GLITE

--- manifests/config.pp
+++ manifests/config.pp
@@ -167,7 +167,7 @@ class arc_ce::config (
     file { '/etc/arc/runtime/ENV/GLITE':
       ensure  => present,
       source  => "puppet:///modules/${module_name}/RTEs/GLITE",
-      require => File['/etc/arc/runtime/'],
+      require => File['/etc/arc/runtime/ENV'],
     }
 
     file { '/etc/arc/runtime/ENV/PROXY':
